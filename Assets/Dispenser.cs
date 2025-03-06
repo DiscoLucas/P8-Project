763daa165e7540163569af56a79bdca19bc0f279
BingoMango <103103876+HurtigSkelet@users.noreply.github.com>
2025-03-06 10:43:47 +0100

    You can now throw cucumbers everywhere

--- Assets/Dispenser.cs
+++ Assets/Dispenser.cs
@@ -1,37 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class Dispenser : MonoBehaviour
+public class DispenseObjectFunction : MonoBehaviour
 {
     public GameObject DispensedObject;
-    XRGrabInteractable grab;
-    private bool hasBeenGrabbed = false; // Flag to track if the hand has grabbed the dispenser
+    XRGrabInteractable handle;
 
     void Start()
     {
-        grab = GetComponent<XRGrabInteractable>();
+        handle = GetComponent<XRGrabInteractable>();
     }
 
-    private void OnTriggerStay(Collider other)
+    void Update()
     {
         // Check if it's a VR hand and it has not been grabbed yet
-        if (other.CompareTag("VR Hand") && !hasBeenGrabbed && grab.isSelected)
+        if (handle.isSelected)
         {
-            Debug.Log("ugga");
-            hasBeenGrabbed = true; // Set the flag to true so it won't trigger again
-            GameObject DispensedObj = Instantiate<GameObject>(DispensedObject, other.transform.position,other.transform.rotation); //I want to rename DispensedObj
-
-            
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        // Reset the flag when the hand exits the dispenser, allowing the message to trigger again when the hand re-enters
-        if (other.CompareTag("VR Hand"))
-        {
-            hasBeenGrabbed = false;
+            GameObject DispensedObj = Instantiate<GameObject>(DispensedObject,this.transform.position, this.transform.rotation);
+            Rigidbody drb = DispensedObj.GetComponent<Rigidbody>();
+            drb.isKinematic = false;
+            drb.useGravity = true;
+            Destroy(this);
         }
     }
 }
