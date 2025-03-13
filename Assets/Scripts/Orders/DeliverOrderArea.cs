369cca946368dc738212f98f19afffaa622fbc3a
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2025-03-13 10:49:35 +0100

    Working on basic gameloop

--- Assets/Scripts/Orders/DeliverOrderArea.cs
+++ Assets/Scripts/Orders/DeliverOrderArea.cs
@@ -8,6 +8,7 @@ public class DeliverOrderArea : MonoBehaviour
     private string glassTag = "Glass";
     public UnityEvent<Order> orderDeliverede = new UnityEvent<Order>();
     public Order order;
+    public GameObject tex_feild;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,5 +18,10 @@ public class DeliverOrderArea : MonoBehaviour
             orderDeliverede.Invoke(order);
             Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(tex_feild);
     }
 }
--- Assets/Scripts/Orders/OrderManager.cs
+++ Assets/Scripts/Orders/OrderManager.cs
@@ -4,7 +4,7 @@ using AYellowpaper.SerializedCollections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
+using TMPro;
 /// <summary>
 /// The manager the controlls the order
 /// </summary>
@@ -13,9 +13,10 @@ public class OrderManager : MonoBehaviour
     [SerializedDictionary("Id", "Order")]
     public SerializedDictionary<string, Order> currentOrderList;
     public RecipeManager recipeManager;
-    public GameObject deliverArea_Prefab;
-    public List<Transform> availableSpawnPoints = new List<Transform>(); 
-
+    public GameObject deliverArea_Prefab,Text_prefab,parent_to_text;
+    public List<Transform> availableSpawnPoints = new List<Transform>();
+    public TMP_Text scoreCounter;
+    public float totalScore = 0;
 
     private void Start()
     {
@@ -33,6 +34,8 @@ public class OrderManager : MonoBehaviour
         List<IngredientBase> order_List = order.containerLimited.getIngreidentsAsOrderedeList();
         float timeTaken = Time.timeSinceLevelLoad - order.startPoint;
         float score = recipeManager.compareTwoIngridienseList(ideal_List, order_List, timeTaken, recipe.expectedTime, out int wrongIngreidentCount, out float totalDeviation, out float totalOverpour, out float totalUnderpour);
+        totalScore += score;
+        scoreCounter.text = $"Score: {totalScore}";
         Destroy(order.containerLimited.gameObject);
         availableSpawnPoints.Add(order.location);
         currentOrderList.Remove(order.orderID);
@@ -54,9 +57,17 @@ public class OrderManager : MonoBehaviour
 
         string orderName = recipe.Name + Time.timeSinceLevelLoad;
         Order order = new Order(keyRecipe, orderName, spawnPoint);
-
+        GameObject text_probemt = Instantiate(Text_prefab, parent_to_text.transform);
+        text_probemt.SetActive(true);
         GameObject deliverArea = Instantiate(deliverArea_Prefab, spawnPoint.position,Quaternion.identity);
         DeliverOrderArea deliverOrderArea = deliverArea.GetComponent<DeliverOrderArea>();
+        string recipe_promt = orderName + " - " + recipe.Name + ": ";
+        foreach (IngredientBase ingredient in recipe.ingredients) {
+            recipe_promt += "[" + ingredient.Name + " " + ingredient.Amount + "]";
+        }
+        Debug.Log(recipe_promt);
+        text_probemt.GetComponent<TMP_Text>().text = recipe_promt;
+        deliverOrderArea.tex_feild = text_probemt;
         deliverOrderArea.order = order;
         deliverOrderArea.orderDeliverede.AddListener(finnishOrder);
         currentOrderList.Add(orderName, order);
