286c96d8592f19d604b1ac0c47c33fb1f3c6c6c5
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2025-04-24 17:32:01 +0200

    Fixed the strainer !

--- Assets/Scripts/Drink_interaction/ShakerLiquidPourer.cs
+++ Assets/Scripts/Drink_interaction/ShakerLiquidPourer.cs
@@ -1,8 +1,15 @@
 using Assets.Scripts.Ingridence;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class ShakerLiquidPourer : LiquidPourer
 {
+    [SerializeField]
+    XRSocketInteractor lowerSocket;
+    [SerializeField]
+    XRSocketInteractor upperSocket;
+
     [Header("Shaker parts")]
     [SerializeField]
     bool lower_cap = false;
@@ -11,6 +18,74 @@ public class ShakerLiquidPourer : LiquidPourer
     [SerializeField]
     bool strainer = false;
     [SerializeField] protected Transform pourPointClosed;
+
+    private void OnEnable()
+    {
+        if (lowerSocket != null)
+        {
+            lowerSocket.selectEntered.AddListener(OnLowerSocketSelectEntered);
+            lowerSocket.selectExited.AddListener(OnLowerSocketSelectExited);
+        }
+
+        if (upperSocket != null)
+        {
+            upperSocket.selectEntered.AddListener(OnUpperSocketSelectEntered);
+            upperSocket.selectExited.AddListener(OnUpperSocketSelectExited);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (lowerSocket != null)
+        {
+            lowerSocket.selectEntered.RemoveListener(OnLowerSocketSelectEntered);
+            lowerSocket.selectExited.RemoveListener(OnLowerSocketSelectExited);
+        }
+
+        if (upperSocket != null)
+        {
+            upperSocket.selectEntered.RemoveListener(OnUpperSocketSelectEntered);
+            upperSocket.selectExited.RemoveListener(OnUpperSocketSelectExited);
+        }
+    }
+
+    private void OnLowerSocketSelectEntered(SelectEnterEventArgs args)
+    {
+        if(args.interactableObject.transform.gameObject.tag == "Strainer")
+        {
+            set_strainer(true);
+            GameObject interactingObject = args.interactableObject.transform.gameObject;
+            Debug.Log("Attached object's tag is: " + interactingObject.name + " and name is: " + args.interactorObject.transform.name);
+
+            if (interactingObject.tag == "Strainer")
+            {
+                Debug.Log("Strainer attached.");
+            }
+        }
+        else
+        {
+            set_lower_cap(true);
+            Debug.Log("Strainer detached.");
+        }
+    }
+
+    private void OnLowerSocketSelectExited(SelectExitEventArgs args)
+    {
+        set_lower_cap(false);
+        set_strainer(false);
+        set_lower_cap(false);
+    }
+
+    private void OnUpperSocketSelectEntered(SelectEnterEventArgs args)
+    {
+        set_upper_cap(true);
+    }
+
+    private void OnUpperSocketSelectExited(SelectExitEventArgs args)
+    {
+        set_upper_cap(false);
+    }
+
     public void set_lower_cap(bool value)
     {
         lower_cap = value;
@@ -21,7 +96,7 @@ public class ShakerLiquidPourer : LiquidPourer
         upper_cap = value;
     }
 
-        public void set_strainer(bool value)
+    public void set_strainer(bool value)
     {
         strainer = value;
     }
@@ -33,7 +108,7 @@ public class ShakerLiquidPourer : LiquidPourer
 
     internal override Transform getPourPoint()
     {
-        if(!lower_cap)
+        if (!lower_cap)
             return pourPoint;
         else
             return pourPointClosed;
@@ -41,8 +116,8 @@ public class ShakerLiquidPourer : LiquidPourer
 
     internal override IngredientBase getIngredientBase()
     {
-        if (strainer){
-
+        if (strainer)
+        {
             IngredientBase ingredientBase = base.getIngredientBase();
             ingredientBase.step.action = DrinkAction.Strained;
             return ingredientBase;
@@ -56,6 +131,6 @@ public class ShakerLiquidPourer : LiquidPourer
         bool haveEnoughtLiqquid = false;
         if (liquidContainer != null)
             haveEnoughtLiqquid = liquidContainer.canPoourer();
-        return isPouring && haveEnoughtLiqquid && !upper_cap;
+        return isPouring && haveEnoughtLiqquid && !(upper_cap && !strainer);
     }
 }
